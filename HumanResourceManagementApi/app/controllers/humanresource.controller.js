8138807f8231c6ba0d1cba67e69888f8718e75e9
brosminn <brosminn@gmail.com>
2019-05-27 00:13:04 -0600

    Update humanresource.controller.js

--- HumanResourceManagementApi/app/controllers/humanresource.controller.js
+++ HumanResourceManagementApi/app/controllers/humanresource.controller.js
@@ -3,84 +3,91 @@ const HumanResourceManager = require('../managers/humanresource.manager.js');
 module.exports = {
 
     //Fetch all employees info
-    getAllEmployeesInfo: function (req, res) {
-        HumanResourceManager.getAllEmployeesInfo(req, function (err, data) {
+    getAllEmployeesInfo: function(req, res) {
+        HumanResourceManager.getAllEmployeesInfo(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
-            }
-            else {
+            } else {
                 res.send(data);
             }
         })
     },
 
     //Fetch employee info based on id or name
-    getEmployeeInfoBasedOnIdOrName: function (req, res) {
-        HumanResourceManager.getEmployeeInfoBasedOnIdOrName(req, function (err, data) {
+    search: function(req, res) {
+        HumanResourceManager.getEmployeeInfoBasedOnIdOrName(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
+            } else {
+                res.send(data);
             }
-            else {
+        })
+    },
+
+    //Fetch employee info based on id or name
+    getEmployeeById: function(req, res) {
+        HumanResourceManager.getEmployeeById(req, function(err, data) {
+            if (err) {
+                res.status(500).send({
+                    message: err || "Some error occurred."
+                });
+            } else {
                 res.send(data);
             }
         })
     },
 
     //Create employee
-    createEmployeeInfo: function (req, res) {
-        HumanResourceManager.createEmployeeInfo(req, function (err, data) {
+    createEmployeeInfo: function(req, res) {
+        HumanResourceManager.createEmployeeInfo(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
-            }
-            else {
+            } else {
                 res.send(data);
             }
         })
     },
 
     //Upate employee info
-    updateEmployeeInfo: function (req, res) {
-        HumanResourceManager.updateEmployeeInfo(req, function (err, data) {
+    updateEmployeeInfo: function(req, res) {
+        HumanResourceManager.updateEmployeeInfo(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
-            }
-            else {
+            } else {
                 res.send(data);
             }
         })
     },
 
     //Delete employee info based on employeeId
-    deleteEmployeeInfo: function (req, res) {
-        HumanResourceManager.deleteEmployeeInfo(req, function (err, data) {
+    deleteEmployeeInfo: function(req, res) {
+        HumanResourceManager.deleteEmployeeInfo(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
-            }
-            else {
+            } else {
                 res.send(data);
             }
         })
     },
 
     //Fetch all managers info
-    getAllManagersInfo: function (req, res) {
-        HumanResourceManager.getAllManagersInfo(req, function (err, data) {
+    getAllManagersInfo: function(req, res) {
+        HumanResourceManager.getAllManagersInfo(req, function(err, data) {
             if (err) {
                 res.status(500).send({
                     message: err || "Some error occurred."
                 });
-            }
-            else {
+            } else {
                 res.send(data);
             }
         })
